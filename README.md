857124bb17f0e2ebef5e99c5188cd96881b0cffc
UZMAFARZEEN <uzmafarzeen23@gmail.com>
2026-05-12 13:07:01 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# CloudNotes-Lite
-Cloud Notes Lite — A lightweight real-time cloud notes application built with Firebase Firestore for instant note synchronization, management, and deletion.
+Project Title: CloudNotes Lite: Real-Time Cloud-Based Note Synchronization System
+
+Description:
+CloudNotes Lite is a lightweight web-based productivity tool designed to help students quickly add, manage, and delete notes in a simple and efficient way. It is browser-based and requires no installation.
+
+Features:
+- Add notes
+- Delete notes
+- Instant display of notes
+- Persistent storage using cloud (Firebase Firestore)
+- Real-time synchronization across sessions
+
+Technologies Used:
+- HTML
+- CSS
+- JavaScript
+- Firebase Firestore (Cloud Database)
+
+How to Run:
+1. Open index.html in any web browser
+2. Enter a note and click "Add Note"
+3. Notes are stored in the cloud and remain even after refreshing the page
+
+Cloud Integration:
+This project uses Firebase Firestore, a cloud-based NoSQL database, to store and access notes in real time. It also supports real-time synchronization, allowing updates to be reflected instantly across sessions.
+
+Demo Video:
+demo.mp4
+
+Author:
+Syed Uzma Farzeen
---
+++ script.js
@@ -0,0 +1,56 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC88XrgxqFSn_ltjPyy37UIK5uTGoIxiyE",
+  authDomain: "cloudnotes-7060c.firebaseapp.com",
+  projectId: "cloudnotes-7060c",
+  storageBucket: "cloudnotes-7060c.firebasestorage.app",
+  messagingSenderId: "296564127708",
+  appId: "1:296564127708:web:15eedd4ba198c50c938119",
+  measurementId: "G-CWPGQ7MZLS"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+function addNote() {
+  let input = document.getElementById("noteInput");
+  let note = input.value.trim();
+
+  if (note === "") return;
+
+  db.collection("notes").add({
+    text: note
+  })
+  .then(() => {
+    input.value = "";
+  })
+  .catch(error => {
+    console.error("Error adding note:", error);
+  });
+}
+
+function loadNotes() {
+  let list = document.getElementById("notesList");
+
+  db.collection("notes").onSnapshot(snapshot => {
+    list.innerHTML = "";
+
+    snapshot.forEach(doc => {
+      let li = document.createElement("li");
+      li.innerHTML =
+        doc.data().text +
+        ` <button onclick="deleteNote('${doc.id}')">Delete</button>`;
+      list.appendChild(li);
+    });
+  }, error => {
+    console.error("Error loading notes:", error);
+  });
+}
+
+function deleteNote(id) {
+  db.collection("notes").doc(id).delete()
+    .catch(error => {
+      console.error("Error deleting note:", error);
+    });
+}
+
+loadNotes();
